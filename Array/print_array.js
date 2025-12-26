4dc0d601c944295c459693f76033aeabede07a05
Skulldued <skulldued30@gmail.com>
2025-12-26 22:50:25 +0530

    Error while sum of aRRAY

--- Array/print_array.js
+++ Array/print_array.js
@@ -3,9 +3,9 @@ function print(array, count, index) {
     return;
   }
   console.log(array[index]);
-  print(array, count, index + 1);
+  return print(array, count, index + 1);
 }
 let array = [3, 7, 6, 2, 8];
-let count = array.length ;
+let count = array.length ; 
 let index = 0;
 print(array, count, index);
---
+++ Array/sum_array.js
@@ -0,0 +1,13 @@
+function sum(array, index) {
+  if (index == -1) {
+    return;
+  }
+
+  return array[index] + sum(array, index - 1);
+}
+
+let array = [3, 4, 5, 8, 2];
+let index = array.length - 1;
+
+let sum_of_array = sum(array, index);
+console.log(sum_of_array);
